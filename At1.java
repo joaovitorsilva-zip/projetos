70b8bb260519b784a790eb9afb9f2efa4299b07c
João Vitor <joaovitorsoaressilva2004@gmail.com>
2026-03-22 09:56:59 -0300

    Exercicio 1

---
+++ At1.java
@@ -0,0 +1,10 @@
+public class At1   {
+    public static void main (String[] args ) {
+        String nome = "João";
+        int idade = 18;
+        double altura = 1.85;
+        System.out.println("nome:"+ nome);
+        System.out.println("idade:"+ idade);
+        System.out.println("altura:"+ altura);
+    }
+}
