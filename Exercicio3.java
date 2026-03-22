8121c17be7389cffd6ae550be0ba2835e91ba3f1
João Vitor <joaovitorsoaressilva2004@gmail.com>
2026-03-22 11:37:29 -0300

    Add files via upload

---
+++ Exercicio3.java
@@ -0,0 +1,8 @@
+public class Exercicio3 {
+    public static void main(String[]args){
+        double nota1 = 4.75;
+        double nota2 = 8.25;
+        double media = (nota1 + nota2) / 2;
+        System.out.println("Media: "+ media);
+    }
+}
